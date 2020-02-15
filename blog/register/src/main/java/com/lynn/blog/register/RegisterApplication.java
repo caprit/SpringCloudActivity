592216ad805a5932bcccc5b76e3ef002520ea632
caprit <caprit.jiang@hotmail.com>
2020-02-15 18:15:48 +0800

    20200215

--- blog/register/src/main/java/com/lynn/blog/register/RegisterApplication.java
+++ blog/register/src/main/java/com/lynn/blog/register/RegisterApplication.java
@@ -1,14 +1,18 @@
 package com.lynn.blog.register;
 
-import com.lynn.blog.common.app.Application;
+//import com.lynn.blog.common.app.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringCloudApplication
-public class RegisterApplication extends Application {
+//@SpringCloudApplication
+@SpringBootApplication
+public class RegisterApplication /*extends Application*/ {
 
     public static void main(String[] args) {
-        Application.startup(RegisterApplication.class,args);
+        /*Application.startup*/
+        SpringApplication.run(RegisterApplication.class,args);
     }
 }
